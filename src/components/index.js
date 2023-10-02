fd86aab3e4e5ae79b0b835916a94b092881a4440
Suyog Muley <53749026+Suyog28@users.noreply.github.com>
2023-10-02 18:05:02 +0530

    Nike Clone Website

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Button from "./Button";
+import Nav from "./Nav";
+import ShoesCard from "./ShoesCard";
+import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
+
+export {
+    Button,
+    Nav,
+    ShoesCard,
+    PopularProductCard,
+    ServiceCard,
+    ReviewCard,
+}
---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import CustomerReview from "./CustomerReview";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+
+
+export {
+    Hero,
+    Footer,
+    CustomerReview,
+    PopularProducts,
+    Services,
+    SpecialOffers,
+    Subscribe,
+    SuperQuality
+}
